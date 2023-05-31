4448090b223970c40b11bef75a64e577206acd94
programmingherorubel <programmingherorubel@gmail.com>
2023-06-01 04:32:05 +0600

    first commit .

--- src/Pages/LoadData.jsx
+++ src/Pages/LoadData.jsx
@@ -16,7 +16,7 @@ const LoadData = () => {
         <Navbar/>
             <div className='mt-10 grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1'>
                 {
-                    fkData.map((item,index)=> <div className='m-4 p-3 shadow-lg ' key={index}>
+                    fkData.map((item,index)=> <div className='mt-10 mx-4 p-3 shadow-lg ' key={index}>
                         <div className='mt-5'>
                             <h4 className='text-center text-2xl text-emerald-400'>{item.name}</h4>
                             <p className='font-semibold'>{item.email}</p>
